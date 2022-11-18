0e46dbda0aba4b3b87db9fa7d9315223f95cdf76
Jirasak07 <bee.110243@gmail.com>
2022-11-18 15:21:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
 var express = require("express");
 var app = express();
-var bodyParser = require('body-parser')
-var jsonParser = bodyParser.json();
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-var jwt = require("jsonwebtoken");
-const secret = "JirasakPRJ2022KPRU";
-app.use(express.static('public'));
 var PORT = env.PORT || 5409;
-var cors = require("cors");
-var sql = require("mssql");
-
-var configdb = {
-  user: "merlin",
-  password: "Admin5409",
-  server: "thaiserve.database.windows.net",
-  database: "dbproject",
-};
-app.use(cors());
 app.listen(PORT, function () {
   console.log("app runing port ", { PORT });
 });
