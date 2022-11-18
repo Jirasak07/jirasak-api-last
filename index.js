7e099d42e20f7f97e76c850313f105dfbf4e1324
Jirasak07 <bee.110243@gmail.com>
2022-11-18 15:20:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const saltRounds = 10;
 var jwt = require("jsonwebtoken");
 const secret = "JirasakPRJ2022KPRU";
 app.use(express.static('public'));
-var PORT = 5409;
+var PORT = env.PORT || 5409;
 var cors = require("cors");
 var sql = require("mssql");
 
@@ -24,39 +24,4 @@ app.listen(PORT, function () {
 app.get("/", function (req, res, next) {
   res.send("API is Run!!");
 });
-
-// test Api
-
-app.post("/conn",jsonParser, function (req, res, next) {
-  // connect to your database
-  sql.connect(configdb, function (err) {
-    if (err) console.log(err);
-    // create Request object
-    var request = new sql.Request();
-    // query to the database and get the records
-    // var pid = 1234
-    // var pname = 'req.body.pname'
-    var pid = req.body.pid
-    var pname = req.body.pname
-    request.query(`UPDATE dbo.product SET pid = ${pid},pname= '${pname}' WHERE pid = 1234   `, function (err, results) {
-    // request.query(`INSERT INTO dbo.product (pid,pname) VALUES (${pid},'${pname}')  `, function (err, results) {
-    // request.query(`select * from dbo.product where pid = ${value} `, function (err, results) {
-      if (err) console.log(err);
-      // send records as a response
-      // res.send(results.recordset);
-      res.json("success")
-
-      console.log(results.recordset);
-    });
-  });
-});
-// ${req.body.pid}
-// end test Api
-
-// Product
-
-//
-
-// end Product
-
 module.exports = app;
